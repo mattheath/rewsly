b29755c17c0961461f71ffbea8d539bf13222b85
Matt Heath <matt@mattheath.com>
2014-07-26 16:43:04 +0100

    Add story editing

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :check_ownership, only: [:edit, :update]
 
   def index
     @stories = params[:q] ? Story.search_for(params[:q]) : Story.all
@@ -24,4 +25,29 @@ class StoriesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    safe_story_params = params.require(:story).permit(:title, :link, :category)
+    @story.update safe_story_params
+
+    if @story.save
+      redirect_to @story
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def check_ownership
+      @story = Story.find params[:id]
+      if @story.user.try(:id) != current_user.id
+        flash[:alert] = "You do not have permission to edit this story"
+        redirect_to @story
+      end
+    end
+
 end
